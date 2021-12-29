f97142d3ca1058365919ac361054254ad8996fc6
anjalimishra9 <anjalixy@gmail.com>
2021-12-29 15:31:17 +0100

    Bugs are fixed(TAS7 & TAS 14)

--- src/main/webapp/resources/js/calculator.js
+++ src/main/webapp/resources/js/calculator.js
@@ -1,49 +1,64 @@
- //function that display value
+//function that display value
 function dis(val) {
-     document.getElementById("result").value+=val;
-}
-   
- //function that clear the display
-function clr() {
-     document.getElementById("result").value = "";
+	document.getElementById("result").value += val;
 }
 
-function escapeHtml(unsafe)
-{
-    return unsafe
-         .replaceAll("+", "plus")
-         .replaceAll("-", "minus")
-		 .replaceAll("*", "multiply")
-		 .replaceAll("/", "divide");
- }
+//function to allow only numeric and +-*/. characters
+function isNumberKey(evt) {
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	var isValid = ((charCode >= 48 && charCode <= 57) || (charCode >= 42 && charCode <= 46));
+	//alert('charCode:'+ charCode + ',isValid:' + isValid);
+	return isValid;
+}
+
+//function that clear the display
+function clr() {
+	document.getElementById("result").value = "";
+}
+
+function escapeHtml(unsafe) {
+	return unsafe
+		.replaceAll("+", "plus")
+		.replaceAll("-", "minus")
+		.replaceAll("*", "multiply")
+		.replaceAll("/", "divide");
+}
 
 function calculate() {
-	var expr = document.getElementById("result").value;
- 	expr = escapeHtml(expr.toString());
-    console.log('expr: ' + expr);
+	var expr = document.getElementById("result").value;	
+	var pattern = /^[\/|_]|[~"#%&*:<>?\\{|}]|[\+\-\*\.\/]{2,}|[\+\-\*\.\/|.]$/;
+	var isValidExpression = !pattern.test(expr);
+	//alert(expr + ':' + isValidExpression);
+	if (isValidExpression) {
+		expr = escapeHtml(expr.toString());
+		console.log('expr: ' + expr);
 
-    $.ajax({
-        type: "GET",
-        contentType: "application/json",
-        url: getContextPath() + "/api/calculate?expr='" + expr + "'",
-        timeout: 30000,
-        success: function (data) {
-           console.log('success: ', data);
-           document.getElementById("result").value = data;
-        },
-        error: function (e) {
-            console.log('error: ', e);
+		$.ajax({
+			type: "GET",
+			contentType: "application/json",
+			url: getContextPath() + "/api/calculate?expr='" + expr + "'",
+			timeout: 30000,
+			success: function(data) {
+				console.log('success: ', data);
+				document.getElementById("result").value = data;
+			},
+			error: function(e) {
+				console.log('error: ', e);
 
-        },
-        done: function (e) {
-            console.log('done');
-        }
+			},
+			done: function(e) {
+				console.log('done');
+			}
 
-    })
+		})
+	} else {
+		alert("Found Invalid Expression:" + expr + "!");
+		clr();
+	}
 
 
 }
 
 function getContextPath() {
-    return window.location.pathname.substring(0, window.location.pathname.indexOf("/", 2));
+	return window.location.pathname.substring(0, window.location.pathname.indexOf("/", 2));
 }
